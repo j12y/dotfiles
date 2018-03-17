f88546ae1c788b78f2bf22736ce21dc31258e5bb
Jayson DeLancey <jayson.delancey@gmail.com>
2018-03-17 10:10:12 -0700

    virtualenv on osx

--- python/virtualenv.bash
+++ python/virtualenv.bash
@@ -1,4 +1,7 @@
 export WORKON_HOME=~/.env
 mkdir -p $WORKON_HOME
-export PROJECT_HOME=~/p
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+export PROJECT_HOME=~/w
+# source /usr/local/bin/virtualenvwrapper_lazy.sh
+export VIRTUALENVWRAPPER_SCRIPT=~/Library/Python/2.7/bin/virtualenvwrapper.sh
+source ~/Library/Python/2.7/bin/virtualenvwrapper_lazy.sh
+alias cdp=cdproject
