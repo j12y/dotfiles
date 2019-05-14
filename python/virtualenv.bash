e143aac8c9b31539f289a5f104e64bbfe33f14e7
j12y <jayson.delancey@gmail.com>
2019-05-14 14:28:57 -0700

    fix for python3 virtualenvs

--- python/virtualenv.bash
+++ python/virtualenv.bash
@@ -1,7 +1,14 @@
-export WORKON_HOME=~/.env
+export WORKON_HOME=$HOME/.env
 mkdir -p $WORKON_HOME
-export PROJECT_HOME=~/w
-# source /usr/local/bin/virtualenvwrapper_lazy.sh
-export VIRTUALENVWRAPPER_SCRIPT=~/Library/Python/2.7/bin/virtualenvwrapper.sh
-source ~/Library/Python/2.7/bin/virtualenvwrapper_lazy.sh
+export PROJECT_HOME=$HOME/w
+mkdir -p $PROJECT_HOME
+
+alias python2=/usr/bin/python2.7
+alias python=/usr/local/bin/python3
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+
+# export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
+source /usr/local/bin/virtualenvwrapper_lazy.sh
 alias cdp=cdproject
