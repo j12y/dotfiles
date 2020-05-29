5a7cd800a86c7b9c87fe2aab6d4902c676f25e96
j12y <jayson.delancey@gmail.com>
2020-05-29 10:53:01 -0700

    switch to pipenv, osx catalina update

--- python/virtualenv.bash
+++ python/virtualenv.bash
@@ -1,14 +1,23 @@
-export WORKON_HOME=$HOME/.env
-mkdir -p $WORKON_HOME
-export PROJECT_HOME=$HOME/w
-mkdir -p $PROJECT_HOME
 
-alias python2=/usr/bin/python2.7
-alias python=/usr/local/bin/python3
+# 2020-05-29
+# Adding Python 3.7 to path for resolving pipenv and pipenv-resolver
+export PATH="~/Library/Python/3.7/bin:$PATH"
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+
+# 2020-05-29
+# Going to stop using virtualenv for awhile
+
+# export WORKON_HOME=$HOME/.env
+# mkdir -p $WORKON_HOME
+# export PROJECT_HOME=$HOME/w
+# mkdir -p $PROJECT_HOME
+
+# alias python2=/usr/bin/python2.7
+# alias python=/usr/local/bin/python3
+
+# export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+# export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
 
 # export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-source /usr/local/bin/virtualenvwrapper_lazy.sh
-alias cdp=cdproject
+# source /usr/local/bin/virtualenvwrapper_lazy.sh
+# alias cdp=cdproject
