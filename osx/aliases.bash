b1e70449b3ae1ccd6f39cd5a431ff83ecc12ae8d
j12y <jayson.delancey@gmail.com>
2021-11-13 11:32:56 -0800

    updates for macos monterey

--- osx/aliases.bash
+++
@@ -1,3 +0,0 @@
-# Create aliases to make Finder show/hide hidden files
-alias show='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hide='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
--- python/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo easy_install pip
-sudo pip install virtualenvwrapper
-sudo pip install ipython
-sudo pip install flake8
-sudo pip install nose
